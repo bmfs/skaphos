d879219ef4563308bd52aa152d4624a9a58c576c
Bruno Santos <bmfs@eksperimental.net>
2021-03-30 23:48:06 +0100

    Check if directory is empty before creating

--- pkg/creator/creator.go
+++ pkg/creator/creator.go
@@ -2,6 +2,7 @@ package creator
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -22,11 +23,10 @@ func Create(source, destination string) error {
 	}
 	defer os.Remove(tempDir)
 
-	if isGit(source) {
-		// clone
+	if isGit(source) { // clone
 		git.Clone(source, tempDir)
 	} else if !pathExists(source) {
-		return fmt.Errorf("source path is not valid")
+		return fmt.Errorf("source path does not exist")
 	}
 
 	transformFile := path.Join(tempDir, "transformations.yml")
@@ -35,8 +35,14 @@ func Create(source, destination string) error {
 		return fmt.Errorf("invalid destination")
 	}
 
+	exists := pathExists(destination)
+	empty, _ := isEmpty(destination)
+	if exists && !empty {
+		return fmt.Errorf("destination is not empty")
+	}
+
 	if !pathExists(transformFile) {
-		return fmt.Errorf("project does not have a transformation.yml")
+		return fmt.Errorf("source does not have a `transformations.yml` file")
 	}
 
 	return generator.Generate(transformFile, tempDir, destination, []string{})
@@ -53,8 +59,22 @@ func pathExists(source string) bool {
 		return false
 	}
 
-	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+	if _, err = os.Stat(absPath); err != nil {
 		return false
 	}
 	return true
+}
+
+func isEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
 }
